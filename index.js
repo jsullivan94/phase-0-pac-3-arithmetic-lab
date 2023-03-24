4bd977461f25f099d662162395df6790be5d5d36
James Sullivan <jsullivan8794@gmail.com>
2023-03-24 17:25:38 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a -b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+let number = 10;
+function increment(number) {
+    return (number +=1);
+}
+function decrement(number) {
+    return (number -=1);
+}
+let n = "10";
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+let num  = "5.2"
+function preserveDecimal(num) {
+    return parseFloat(num, 10);
+}
